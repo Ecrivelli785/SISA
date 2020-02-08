12a2ee6a8c0b8bb227d8d11935022f011fab2444
mdiaz <mario.a.diaz@live.com.ar>
2020-02-08 13:23:43 -0300

    modificaciones en schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -52,8 +52,8 @@ ActiveRecord::Schema.define(version: 2020_02_01_190541) do
     t.string "rubro"
     t.boolean "estado"
     t.string "estado_mkt"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.date "created_at", null: false
+    t.date "updated_at", null: false
   end
 
   create_table "tecnicos", force: :cascade do |t|
