c1316776321ed97ca8fc65bc93c4177cbca1bc75
enzo crivelli <enzo_crivelli97@hotmail.com>
2020-02-08 22:01:48 -0300

    certificados front end y arreglé las rutas y los id

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,4 +13,13 @@ class PagesController < ApplicationController
     @results = Cliente.all.where("lower(nombre) LIKE :search OR lower(apellido) LIKE :search OR lower(barrio) LIKE :search OR lower(CUIT) LIKE :search OR lower(correo) LIKE :search", search: "%#{@parameter}%")
   end
   end
+
+  def searchCertificado
+  if params[:search].blank?
+    redirect_to(certificados_path, alert: "Empty field!") and return
+  else
+    @parameter = params[:search].downcase
+    @results = Certificados.all.where("lower(tratamiento) LIKE :search OR lower(id_droga) LIKE :search OR lower(id_vectores) LIKE :search OR lower(fecha_vencimientos) LIKE :search OR lower(id_cliente) LIKE :search", search: "%#{@parameter}%")
+  end
+  end
 end
