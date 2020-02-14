9b41927f6107cae04c28ef9a369902f9612188ed
enzo crivelli <enzo_crivelli97@hotmail.com>
2020-02-14 13:35:57 -0300

    fixed the error with delete in client view

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -29,7 +29,7 @@ class ClientesController < ApplicationController
 
     respond_to do |format|
       if @cliente.save
-        format.html { redirect_to @cliente, notice: 'Cliente was successfully created.' }
+        format.html { redirect_to @cliente, notice: 'El cliente fue creado.' }
         format.json { render :show, status: :created, location: @cliente }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class ClientesController < ApplicationController
   def update
     respond_to do |format|
       if @cliente.update(cliente_params)
-        format.html { redirect_to @cliente, notice: 'Cliente was successfully updated.' }
+        format.html { redirect_to @cliente, notice: 'EL cliente fue actualizado.' }
         format.json { render :show, status: :ok, location: @cliente }
       else
         format.html { render :edit }
@@ -56,10 +56,7 @@ class ClientesController < ApplicationController
   # DELETE /clientes/1.json
   def destroy
     @cliente.destroy
-    respond_to do |format|
-      format.html { redirect_to clientes_url, notice: 'Cliente was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to clientes_path
   end
 
   private
