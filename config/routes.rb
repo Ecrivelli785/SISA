d299d7a2c0381cda4d4344c949176d54528bcb54
enzo crivelli <enzo_crivelli97@hotmail.com>
2020-02-15 10:24:57 -0300

    buscador de certificados falta compeltar

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :certificados
   resources :clientes
   get '/search' => 'pages#search', :as => 'search_page'
+  get '/searchcertificado' => 'pages#searchcertificado', :as => 'searchcertificado_page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
